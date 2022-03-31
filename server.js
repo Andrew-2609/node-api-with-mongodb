cac72342b9fff9c37f2ebaf7e008072691c46187
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-31 20:33:47 -0300

    Feat: replace 'http' implementation with 'express' one from app.js

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const http = require('http');
+import app from './src/app.js';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const routes = {
-    '/': 'NodeJS course',
-    '/books': 'Welcome to the Books page',
-    '/authors': 'Welcome to the Authors page'
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(routes[req.url] || 'Not Found, buddy :/');
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`)
 });
