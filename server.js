454eac5cf406b9b837620e1620c50f8c7a3e2f20
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-31 20:15:31 -0300

    Feat: add simple routes to server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const http = require('http');
 
 const port = 3000;
 
+const routes = {
+    '/': 'NodeJS course',
+    '/books': 'Welcome to the Books page',
+    '/authors': 'Welcome to the Authors page'
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Curso de NodeJS');
+    res.end(routes[req.url] || 'Not Found, buddy :/');
 });
 
 server.listen(port, () => {
