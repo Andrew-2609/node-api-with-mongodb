60274964e9f126292b08b4e6f18ef4ec13c2ef3f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-03-31 22:30:17 -0300

    Feat: create GET endpoint to retrieve a 'book' by its id

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.get('/books', (req, res) => {
     res.status(200).json(books);
 });
 
+app.get('/books/:id', (req, res) => {
+    const bookId = findBook(req.params.id);
+    res.status(200).json(books[bookId]);
+});
+
 app.post('/books', (req, res) => {
     books.push(req.body);
     res.status(201).send('Book successfully inserted!');
