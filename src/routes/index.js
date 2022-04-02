e08ee45cd462a531a5193eb41fdad1439fdff7a2
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 17:09:27 -0300

    Feat: use routes from bookRoutes.js in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
+import express from 'express';
+import bookRoutes from './bookRoutes.js';
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
         res.send(200).send({ title: 'NodeJS course' });
     });
+
+    app.use(
+        express.json(),
+        bookRoutes
+    );
 };
