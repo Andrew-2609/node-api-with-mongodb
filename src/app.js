5d5825d613c2b1824df11a64fe089791cd6386c8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 17:57:30 -0300

    Refactor: remove all useless code from app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import db from './config/dbConnect.js';
-import books from './models/Book.js';
 import routes from './routes/index.js';
 
 db.on('error', console.log.bind(console, 'Connection Error =('));
@@ -11,15 +10,5 @@ db.once('open', () => {
 const app = express().use(express.json());
 
 routes(app);
-
-app.delete('/books/:id', (req, res) => {
-    const bookId = findBook(req.params.id);
-    books.splice(bookId, 1);
-    res.status(200).send(`Book of id ${bookId + 1} successfully removed!`);
-});
-
-function findBook(id) {
-    return books.findIndex(book => book.id === Number(id));
-}
 
 export default app;
