dbc997817f906811e21309f2b210591394898191
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 16:30:40 -0300

    Feat: replace array object with real data from database in 'GET /books' endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from './config/dbConnect.js';
+import books from './models/Book.js';
 
 db.on('error', console.log.bind(console, 'Connection Error =('));
 db.once('open', () => {
@@ -9,17 +10,19 @@ db.once('open', () => {
 const app = express();
 app.use(express.json());
 
-const books = [
-    { id: 1, title: 'Lord of the Rings' },
-    { id: 2, title: 'Harry Potter' }
-];
+// const books = [
+//     { id: 1, title: 'Lord of the Rings' },
+//     { id: 2, title: 'Harry Potter' }
+// ];
 
 app.get('/', (req, res) => {
     res.status(200).send('NodeJS course');
 });
 
 app.get('/books', (req, res) => {
-    res.status(200).json(books);
+    books.find((err, books) => {
+        res.status(200).json(books);
+    });
 });
 
 app.get('/books/:id', (req, res) => {
