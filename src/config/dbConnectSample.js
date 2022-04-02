5dc13dc3d0505da53aadbcad885ae8c778ed2277
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 16:16:14 -0300

    Chore: turn db variable into a const

--- src/config/dbConnectSample.js
+++ src/config/dbConnectSample.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 
 mongoose.connect('mongodb+srv://<host>:<password>@cluster0.auc6g.mongodb.net/<database>');
 
-let db = mongoose.connection;
+const db = mongoose.connection;
 
 export default db;
