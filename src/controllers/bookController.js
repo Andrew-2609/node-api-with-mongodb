c6eaf1c9e210ee346fdf76085d8b82d4af89a770
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 17:26:48 -0300

    Feat: implement method to register a new book in bookController.js

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -7,7 +7,18 @@ class BookController {
             res.status(200).json(books);
         });
     }
-    
+
+    static registerBook = (req, res) => {
+        const book = new books(req.body);
+        book.save((err) => {
+            if (err) {
+                res.status(500).send({ message: `Failed to register book: ${err.message}` });
+            }
+
+            res.status(201).send(book.toJSON());
+        });
+    }
+
 }
 
 export default BookController;
