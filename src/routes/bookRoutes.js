3aeba549bbaa19357797dae238f8f79b47bf6d5f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 17:42:29 -0300

    Feat: implement route to update a book with updateBook() method of bookController.js

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
     .get('/books', BookController.listBooks)
-    .post('/books', BookController.registerBook);
+    .post('/books', BookController.registerBook)
+    .put('/books/:id', BookController.updateBook);
 
 export default router;
