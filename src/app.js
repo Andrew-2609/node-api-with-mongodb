f248cc032527f2d47d30cc8d7ba000f9ef172778
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 16:16:36 -0300

    Feat: configure basic connection with database in app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 import express from 'express';
+import db from './config/dbConnect.js';
+
+db.on('error', console.log.bind(console, 'Connection Error =('));
+db.once('open', () => {
+    console.log('Successfully connected to the database!');
+});
 
 const app = express();
 app.use(express.json());
