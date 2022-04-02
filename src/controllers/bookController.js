aaa1a79057fdddd53705a68d1279dca8ffefcf22
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-04-02 17:03:17 -0300

    Feat: implement static method listBooks() in bookController.js and export its class

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,2 +1,13 @@
+import books from '../models/Book.js';
+
 class BookController {
+
+    static listBooks = (req, res) => {
+        books.find((err, books) => {
+            res.status(200).json(books);
+        });
+    }
+    
 }
+
+export default BookController;
